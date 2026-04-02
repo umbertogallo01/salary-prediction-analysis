a8df09777302476beb90ebab7ed86a82ad98a115
Umberto Gallo <umberto.gallo2001@gmail.com>
2026-04-02 19:11:00 +0200

    Correzioni directory

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,7 +1,7 @@
 from sklearn.preprocessing import StandardScaler
 
 
-def scale_data(X_train, X_test):
+def scale_data(x_train, x_test):
     scaler = StandardScaler()
     X_train_scaled = scaler.fit_transform(x_train)
     X_test_scaled = scaler.transform(x_test)
